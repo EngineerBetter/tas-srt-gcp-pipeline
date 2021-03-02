e60b45f682f3ec59c2a40c22e83e12a5bb5f3c30
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-03-02 14:06:30 +0000

    add necessary creds from tf into credhub

--- ci/tasks/load-tfvars-into-credhub/task.sh
+++ ci/tasks/load-tfvars-into-credhub/task.sh
@@ -2,4 +2,15 @@
 
 set -euo pipefail
 
-sleep 10
+me="$(cd "$(dirname "$0")" && pwd)"
+
+jq '.stable_config_opsmanager |= fromjson | .stable_config_opsmanager' paving-terraform/metadata >opsman_config.json
+jq '..stable_config_pas |= fromjson | ..stable_config_pas' paving-terraform/metadata >tas_config.json
+
+bosh interpolate "${me}/creds.yml" \
+  -l opsman_config.json \
+  -l tas_config.json \
+  -l project-account-and-bucket-terraform/metadata \
+  -l "tas-srt-gcp-pipeline-repo/vars/${ENV}.yml" >interpolated_creds.yml
+
+credhub import -f interpolated_creds.yml
