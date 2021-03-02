7846418352a66847d8eb1d479eb60d72f863d7f0
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-03-02 14:08:11 +0000

    fix script

--- ci/tasks/load-tfvars-into-credhub/task.sh
+++ ci/tasks/load-tfvars-into-credhub/task.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 me="$(cd "$(dirname "$0")" && pwd)"
 
 jq '.stable_config_opsmanager |= fromjson | .stable_config_opsmanager' paving-terraform/metadata >opsman_config.json
-jq '..stable_config_pas |= fromjson | ..stable_config_pas' paving-terraform/metadata >tas_config.json
+jq '.stable_config_pas |= fromjson | .stable_config_pas' paving-terraform/metadata >tas_config.json
 
 bosh interpolate "${me}/creds.yml" \
   -l opsman_config.json \
