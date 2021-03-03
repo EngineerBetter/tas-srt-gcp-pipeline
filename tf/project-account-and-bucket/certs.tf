cf466e24a56605bf1fe133905c98e3bbb0c59ad3
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-03-03 15:43:30 +0000

    pass gce creds to tf job

--- tf/project-account-and-bucket/certs.tf
+++ tf/project-account-and-bucket/certs.tf
@@ -2,12 +2,12 @@ provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
-resource "tls_private_key" "private_key2" {
+resource "tls_private_key" "private_key" {
   algorithm = "RSA"
 }
 
 resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.private_key2.private_key_pem
+  account_key_pem = tls_private_key.private_key.private_key_pem
   email_address   = "${var.env}-tas@engineerbetter.com"
 }
 
