0635b07abe931102a60e6a358bcf66269f1c80d4
Peter Ellis Jones <pete@peterellisjones.com>
2021-03-03 09:53:20 +0000

    WIP: Set GOOGLE_APPLICATION_CREDENTIALS before running create-vm

--- ci/tasks/write-gcp-creds-to-file/task.sh
+++ ci/tasks/write-gcp-creds-to-file/task.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-echo "${GCP_CREDENTIALS_JSON}" | jq -rc '{"gcp_credentials_json": . | tostring}' >tas-srt-gcp-pipeline-repo/vars/g_creds.yml
+echo "${GCP_CREDENTIALS_JSON}" >tas-srt-gcp-pipeline-repo/vars/gcp_creds.json
