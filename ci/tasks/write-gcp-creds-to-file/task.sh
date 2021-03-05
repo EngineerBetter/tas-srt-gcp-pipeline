6a1a08b76d4bff47a4f4cbe9355059ced24d4b49
Peter Ellis Jones <pete@peterellisjones.com>
2021-03-05 11:33:51 +0000

    Override service account in config/director.yml since OM can't seem to detect it properly

--- ci/tasks/write-gcp-creds-to-file/task.sh
+++ ci/tasks/write-gcp-creds-to-file/task.sh
@@ -3,3 +3,8 @@
 set -euo pipefail
 
 echo "${GCP_CREDENTIALS_JSON}" >tas-srt-gcp-pipeline-repo/vars/gcp_creds.json
+
+gcp_service_account="$(echo "${GCP_CREDENTIALS_JSON}" | jq -er '.client_email')"
+
+# manually set tas-srt-gcp-pipeline-repo/config/director.yml to correct service account
+sed -i '' "s/associated_service_account:.*/associated_service_account: $(${gcp_service_account})/g" "tas-srt-gcp-pipeline-repo/config/director.yml"
