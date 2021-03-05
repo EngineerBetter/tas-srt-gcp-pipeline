4b40b5170579a03f05e65597193942f97ba7d708
Peter Ellis Jones <pete@peterellisjones.com>
2021-03-05 11:43:33 +0000

    sed syntax error

--- ci/tasks/write-gcp-creds-to-file/task.sh
+++ ci/tasks/write-gcp-creds-to-file/task.sh
@@ -7,4 +7,4 @@ echo "${GCP_CREDENTIALS_JSON}" >tas-srt-gcp-pipeline-repo/vars/gcp_creds.json
 gcp_service_account="$(echo "${GCP_CREDENTIALS_JSON}" | jq -er '.client_email')"
 
 # manually set tas-srt-gcp-pipeline-repo/config/director.yml to correct service account
-sed -i '' "s/associated_service_account:.*/associated_service_account: ${gcp_service_account}/g" "tas-srt-gcp-pipeline-repo/config/director.yml"
+sed -i "s/associated_service_account:.\*/associated_service_account: ${gcp_service_account}/g" "tas-srt-gcp-pipeline-repo/config/director.yml"
