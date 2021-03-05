70b5af7316849ec911bfc29ed872b1a569093887
Peter Ellis Jones <pete@peterellisjones.com>
2021-03-05 11:53:53 +0000

    fix director service account

---
+++ ci/tasks/configure-director/task.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+gcp_service_account="$(cat vars/vars/gcp_service_account)"
+
+# manually set tas-srt-gcp-pipeline-repo/config/director.yml to correct service account
+# om does not seem to be able to detect this correctly
+sed -i "s/associated_service_account:.\*/associated_service_account: ${gcp_service_account}/g" "config/config/director.yml"
+
+./platform-automation-tasks/tasks/configure-director.sh
--- ci/tasks/create-vm/task.sh
+++ ci/tasks/create-vm/task.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+# Authenticate with Google before creating VM
 service_account="$(awk '/client_email/{print $2}' "vars/vars/gcp_creds.json" | sed 's/[",]//g')"
 
 gcloud auth activate-service-account "${service_account}" --key-file "vars/vars/gcp_creds.json"
