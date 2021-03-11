e908e573d33ee682057457b5d62fd335ea432641
Cai Cooper <cagiti@users.noreply.github.com>
2021-03-11 20:58:09 +0000

    Filter vm for cloud-platform scope using gclouds filter option

--- ci/tasks/change-opsman-vm-scopes/task.sh
+++ ci/tasks/change-opsman-vm-scopes/task.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+gcloud version
+
 # Authenticate with Google before creating VM
 service_account="$(awk '/client_email/{print $2}' "vars/vars/gcp_creds.json" | sed 's/[",]//g')"
 
 gcloud auth activate-service-account "${service_account}" --key-file "vars/vars/gcp_creds.json"
 
-scope_set=$(gcloud compute instances describe ops-manager-vm --format=json | jq '.serviceAccounts[0].scopes[] | contains("https://www.googleapis.com/auth/cloud-platform")')
+scope_set=$(gcloud compute instances list --filter="serviceAccounts.scopes=https://www.googleapis.com/auth/cloud-platform AND name=ops-manager-vm")
 
 if [[ $scope_set == "true" ]]; then
   echo "Scope 'cloud-platform' already set for Opsman VM instance, skipping..."
